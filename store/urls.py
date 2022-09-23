43a3c9d97a32d3eee535632617c1431c847bb5d9
DENIS BABCHUK <denisbabchuk@MacBook-Pro-DENIS.local>
2022-09-23 12:07:10 +0300

    Done

---
+++ store/urls.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.views import LogoutView
+from django.urls import path
+
+from . import views
+from .views import Login, Register
+
+
+urlpatterns = [
+	path('login/', Login.as_view(), name='login'),
+	path('logout/', LogoutView.as_view(next_page='store'), name='logout'),
+	path('register/', Register.as_view(), name='register'),
+
+	path('', views.store, name="store"),
+	path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
+
+	path('update_item/', views.update_item, name="update_item"),
+	path('process_order/', views.process_order, name="process_order"),
+
+]
---
+++ store/views.py
@@ -0,0 +1,129 @@
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.views.generic.edit import FormView
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.contrib.auth import login
+
+import json
+import datetime
+
+from .models import Customer, Product, Order, OrderItem, ShippingAddress
+from .utils import cart_data, guest_order
+from .forms import CreateUserForm
+
+
+class Login(LoginView):
+    template_name = 'store/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('store')
+
+
+class Register(FormView):
+    template_name = 'store/register.html'
+    form_class = CreateUserForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('store')
+
+    def form_valid(self, form):
+        user = form.save()
+        Customer.objects.create(user=user, name=user.username, email=user.email)
+        if user is not None:
+            login(self.request, user)
+        return super(Register, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('store')
+        return super(Register, self).get(*args, **kwargs)
+
+
+def store(request):
+    data = cart_data(request)
+
+    cart_items = data['cart_items']
+
+    products = Product.objects.all()
+    context = {'products': products, 'cart_items': cart_items}
+    return render(request, 'store/store.html', context)
+
+
+def cart(request):
+    data = cart_data(request)
+
+    cart_items = data['cart_items']
+    order = data['order']
+    items = data['items']
+
+    context = {'items': items, 'order': order, 'cart_items': cart_items}
+    return render(request, 'store/cart.html', context)
+
+
+def checkout(request):
+    data = cart_data(request)
+
+    cart_items = data['cart_items']
+    order = data['order']
+    items = data['items']
+
+    context = {'items': items, 'order': order, 'cart_items': cart_items}
+    return render(request, 'store/checkout.html', context)
+
+
+def update_item(request):
+    data = json.loads(request.body)
+    product_id = data['product_id']
+    action = data['action']
+    print('Action:', action)
+    print('Product:', product_id)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=product_id)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    order_item, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        order_item.quantity = (order_item.quantity + 1)
+    elif action == 'remove':
+        order_item.quantity = (order_item.quantity - 1)
+
+    order_item.save()
+
+    if order_item.quantity <= 0:
+        order_item.delete()
+
+    return JsonResponse('Item was added', safe=False)
+
+
+def process_order(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    else:
+        customer, order = guest_order(request, data)
+
+    total = float(data['form']['total'])
+    order.transaction_id = transaction_id
+
+    if total == order.get_cart_total:
+        order.complete = True
+    order.save()
+
+    if order.shipping:
+        ShippingAddress.objects.create(
+            customer=customer,
+            order=order,
+            address=data['shipping']['address'],
+            city=data['shipping']['city'],
+            state=data['shipping']['state'],
+            zipcode=data['shipping']['zipcode'],
+        )
+
+    return JsonResponse('Payment submitted..', safe=False)
